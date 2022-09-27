b37b260351d5393e1fbdfe8749de67700b75f120
Vinayaka98 <polimeravinayaka@gmail.com>
2022-09-27 08:27:58 -0400

    Closes #2

---
+++ Labs/src/main/java/ca/gbc/comp3095/gbccomp3095petclinic/model/Person.java
@@ -0,0 +1,24 @@
+package ca.gbc.comp3095.gbccomp3095petclinic.model;
+
+public class Person {
+    private String firstName;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLasName() {
+        return lasName;
+    }
+
+    public void setLasName(String lasName) {
+        this.lasName = lasName;
+    }
+
+    private String lasName;
+
+}
---
+++ Labs/src/main/java/ca/gbc/comp3095/gbccomp3095petclinic/model/PetType.java
@@ -0,0 +1,14 @@
+package ca.gbc.comp3095.gbccomp3095petclinic.model;
+
+public class PetType {
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    private String Name;
+
+}
